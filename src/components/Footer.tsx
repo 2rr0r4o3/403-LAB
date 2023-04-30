1bc7b9ed09099c112b9cc43a91bd481ea3f6fab2
Error_403 <darklight2357@icloud.com>
2023-05-01 04:37:48 +0900

    Update Footer.tsx
    Modify contents

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,7 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="max-w-xs ml-auto px-5 p-10">
         <h6 className="font-light text-xs mb-10 lg:mb-0 lg:pr-4">
-          Sample website built with{' '}
-          It archives the records of studying exploit techniques against the Linux kernel and software based on it.
+          403 LAB archives the records of studying exploit techniques against the Linux kernel and software based on it.
         </h6>
       </div>
     </footer>
